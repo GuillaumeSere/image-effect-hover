302cf882a9879292e6d59eba68e25d02b7d94aed
Guillaume Sere <guillaumesere@hotmail.fr>
2022-07-16 11:11:57 +0200

    add readme

---
+++ dist/components/EffectImg.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./effectimg.css");
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const EffectImg = () => {
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: "box",
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {}), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {}), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {}), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {})]
+  });
+};
+
+var _default = EffectImg;
+exports.default = _default;

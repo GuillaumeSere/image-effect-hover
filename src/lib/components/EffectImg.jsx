31a155c5c219cfeff469fe3f663c745eb3bf9270
Guillaume Sere <guillaumesere@hotmail.fr>
2022-07-16 10:48:18 +0200

    first commit

---
+++ src/lib/components/EffectImg.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './effectimg.css';
+
+const EffectImg = () => {
+  return (
+    <div className='box'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+};
+
+export default EffectImg;
